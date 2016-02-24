f22bd3f4d222387b25b84fa76b39fd8201590270
Robert Arnold <robertarnold391@gmail.com>
2016-02-24 01:04:09 +0000

    We're makin a videogam lol

---
+++ EinstienPuzzle/EinstienPuzzle/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinstienPuzzle
+{
+    class Solution
+    {
+
+        Character gulity;
+
+        List<Character> characters = new List<Character>();
+        List<Attribute> attributes = new List<Attribute>();
+
+
+
+    }
+}
