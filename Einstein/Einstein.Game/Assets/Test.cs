53fd228837ff6068f380b6034363b4eb3365cdb4
Robert Arnold <robertarnold391@gmail.com>
2016-11-19 20:13:39 +0000

    Created unity project and set up references

---
+++ Einstein/Einstein.Game/Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Einstein.Contracts.Objects;
+using Einstien;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Clue blah;
+	    Scenario scenario;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
